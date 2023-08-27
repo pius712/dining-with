aa10982cebdad28a32bb75feffc33f7dbd8cf908
pius712 <aksfbsgnlfls@gmail.com>
2023-08-27 13:30:48 +0900

    first commit

---
+++ src/main/java/com/example/diningwith/DiningWithApplication.java
@@ -0,0 +1,13 @@
+package com.example.diningwith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningWithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiningWithApplication.class, args);
+    }
+
+}
