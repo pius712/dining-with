f1b4db48553dd6863d1e30d35586a04f6cc27313
pius712 <aksfbsgnlfls@gmail.com>
2023-09-10 22:58:00 +0900

    fix: club 삭제

--- src/main/java/com/example/diningwith/core/domain/meeting/club/recruit/ClubRecruitmentWriter.java
+++
@@ -1,7 +0,0 @@
-package com.example.diningwith.core.domain.meeting.club.recruit;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class ClubRecruitmentWriter {
-}
--- src/main/java/com/example/diningwith/core/storage/meeting/club/ClubRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.diningwith.core.storage.meeting.club;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClubRepository extends JpaRepository<ClubEntity, Long> {
-}
